f0b013b72e1d79ad8970eea995281fd48e521040
Blake Hieu Nguyen <57298440+nvthblake@users.noreply.github.com>
2020-09-23 20:29:16 -0400

    Change Spice page to Profile page, then reorder pages

--- SmartFridge/app/src/main/java/com/example/smartfridge/ProfileFragment.java
+++ SmartFridge/app/src/main/java/com/example/smartfridge/ProfileFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SpiceFragment#newInstance} factory method to
+ * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SpiceFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class SpiceFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public SpiceFragment() {
+    public ProfileFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class SpiceFragment extends Fragment {
      * @return A new instance of fragment SpiceFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SpiceFragment newInstance(String param1, String param2) {
-        SpiceFragment fragment = new SpiceFragment();
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class SpiceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spice, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
