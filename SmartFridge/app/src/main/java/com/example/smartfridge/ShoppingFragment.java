36a5c16c58b749da2d4da5d906150196d94d2b90
Quang Luong <crzqag@Quangs-MacBook-Pro.local>
2020-09-26 01:28:42 -0400

    Fix fixed height for parent view and add shopping list feature

--- SmartFridge/app/src/main/java/com/example/smartfridge/ShoppingFragment.java
+++ SmartFridge/app/src/main/java/com/example/smartfridge/ShoppingFragment.java
@@ -1,12 +1,34 @@
 package com.example.smartfridge;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +41,10 @@ public class ShoppingFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    ArrayList<String> shoppingList = null;
+    ArrayAdapter<String> adapter = null;
+    ListView lv = null;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -49,16 +75,103 @@ public class ShoppingFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping, container, false);
+        View view = inflater.inflate(R.layout.fragment_shopping, container, false);
+
+        shoppingList = getArrayVal(view.getContext());
+//        Collections.addAll Toothpaste");
+        Collections.sort(shoppingList);
+        adapter = new ArrayAdapter(view.getContext(), android.R.layout.simple_list_item_1, shoppingList);
+        lv = (ListView)view.findViewById(R.id.ListView);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = ((TextView)view).getText().toString();
+                if (selectedItem.trim().equals(shoppingList.get(i).trim())) {
+                    removeElement(selectedItem, i, view);
+                }
+                else {
+                    Toast.makeText(view.getContext(), "Error Removing Element", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener((_view) -> {
+//            Snackbar.make(_view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show();
+            AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+            builder.setTitle("Add Item");
+            final EditText input = new EditText(view.getContext());
+            builder.setView(input);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    shoppingList.add(preferredCase(input.getText().toString()));
+                    Collections.sort(shoppingList);
+                    storeArrayVal(shoppingList, view.getContext());
+                    lv.setAdapter(adapter);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.show();
+        });
+
+        return view;
+    }
+
+    public static String preferredCase(String original) {
+        if (original.isEmpty()) {
+            return original;
+        }
+        return original.substring(0, 1).toUpperCase() + original.substring(1).toLowerCase();
+    }
+
+    public static void storeArrayVal(ArrayList inArrayList, Context context) {
+        Set WhatToWrite = new HashSet(inArrayList);
+        SharedPreferences WordSearchPutPrefs = context.getSharedPreferences("dbArrayValues", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor prefEditor = WordSearchPutPrefs.edit();
+        prefEditor.putStringSet("myArray", WhatToWrite);
+        prefEditor.commit();
+    }
+
+    public static ArrayList getArrayVal(Context dan) {
+        SharedPreferences WordSearchGetPrefs = dan.getSharedPreferences("dbArrayValues", Activity.MODE_PRIVATE);
+        Set tempSet = new HashSet();
+        tempSet = WordSearchGetPrefs.getStringSet("myArray", tempSet);
+        return new ArrayList<>(tempSet);
+    }
+
+    public void removeElement(String selectedItem, final int position, View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setTitle("Remove " + selectedItem + "?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                shoppingList.remove(position);
+                Collections.sort(shoppingList);
+                storeArrayVal(shoppingList, view.getContext());
+                lv.setAdapter(adapter);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.show();
     }
 }
